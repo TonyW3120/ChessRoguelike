3bb8d26aa36299981392efbe1087ba451133e368
TonyW3120 <167535557+TonyW3120@users.noreply.github.com>
2024-05-17 08:47:14 -0400

    Added health attribute to ship

--- ship_class.py
+++ ship_class.py
@@ -1,7 +1,8 @@
 class Ship(object):
-    def __init__(self, position_x, position_y, size, color):
+    def __init__(self, position_x, position_y, size, color, health):
         self.visual = 0
         self.position_x = position_x
         self.position_y = position_y
         self.size = size
         self.color = color
+        self.health = health
